b8e34d9af0edefab53c91a3963b7c7ffcb315d60
碎碎酱 <yinxin630@gmail.com>
2017-03-30 09:54:49 +0800

    feat: modify assets sub directory

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
         env: require('./prod.env'),
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'static',
+        assetsSubDirectory: '.',
         assetsPublicPath: '/',
         productionSourceMap: true,
         productionGzip: false,
