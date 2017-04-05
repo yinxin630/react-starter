01bc8f0926c9c84fdaaec43b0223f62ca072ea12
碎碎酱 <yinxin630@gmail.com>
2017-04-05 10:29:33 +0800

    feat: modify eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
     'rules': {
         'indent': [2, 4],
         'global-require': 0,
+        'camelcase': 0,
+        'brace-style': 0,
+        'no-alert': 0,
 
         'import/no-extraneous-dependencies': 0,
         'import/no-unresolved': 0,
@@ -30,10 +33,17 @@ module.exports = {
         'no-param-reassign': 0,
         'no-restricted-syntax': [2, 'WithStatement'],
         'no-invalid-this': 0,
+        'no-useless-return': 0,
 
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         'react/jsx-filename-extension': 0,
         'react/prefer-stateless-function': 0,
+        'react/require-default-props': 0,
+        'react/no-multi-comp': 0,
+        'react/forbid-prop-types': 0,
+        'react/no-array-index-key': 0,
+        'jsx-a11y/img-has-alt': 0,
+        'jsx-a11y/no-static-element-interactions': 0,
     }
 };
